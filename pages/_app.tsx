1e2c4c3042852f1306e632f1f52e057f07c7a973
elbekjustin <elbekjumanazarov03@gmail.com>
2024-12-29 20:07:00 +0900

    feat: SASS integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { light } from "../scss/MaterialTheme";
 import { useState } from "react";
+import "../scss/app.scss"
 
 export default function App({ Component, pageProps }: AppProps) { // Next.js ilovasi boshqariladi
   // @ts-ignore
